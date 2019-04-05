172a57cd07ff5cca9cad716b10d5d81bc8ef8aef
Peter Cai <peter@typeblog.net>
2019-04-05 16:44:18 +0200

    Initial support for in-display fingerprint sensors
    Change-Id: I52beaf6492b9daa012b5d42b858d552d1e378a2c

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -93,9 +93,14 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_SHOW_CHARGING_ANIMATION       = 44 << MSG_SHIFT;
     private static final int MSG_SHOW_PINNING_TOAST_ENTER_EXIT = 45 << MSG_SHIFT;
     private static final int MSG_SHOW_PINNING_TOAST_ESCAPE     = 46 << MSG_SHIFT;
+<<<<<<< HEAD
     private static final int MSG_TOGGLE_CAMERA_FLASH           = 47 << MSG_SHIFT;
     private static final int MSG_RESTART_UI                    = 48 << MSG_SHIFT;
     private static final int MSG_TOGGLE_SETTINGS_PANEL                  = 100 << MSG_SHIFT;
+=======
+    private static final int MSG_IN_DISPLAY_FINGERPRINT        = 47 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_CAMERA_FLASH           = 48 << MSG_SHIFT;
+>>>>>>> 9e305c9d733... Initial support for in-display fingerprint sensors
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -169,6 +174,7 @@ public class CommandQueue extends IStatusBar.Stub {
         default void onFingerprintHelp(String message) { }
         default void onFingerprintError(String error) { }
         default void hideFingerprintDialog() { }
+        default void handleInDisplayFingerprintView(boolean show, boolean isEnrolling) { }
         default void toggleCameraFlash() { }
         default void restartUI() { }
     }
@@ -565,6 +571,17 @@ public class CommandQueue extends IStatusBar.Stub {
     }
 
     @Override
+    public void handleInDisplayFingerprintView(boolean show, boolean isEnrolling) {
+        synchronized (mLock) {
+            SomeArgs args = SomeArgs.obtain();
+            args.arg1 = show;
+            args.arg2 = isEnrolling;
+            mHandler.obtainMessage(MSG_IN_DISPLAY_FINGERPRINT, args)
+                    .sendToTarget();
+        }
+    }
+
+    @Override
     public void toggleCameraFlash() {
         synchronized (mLock) {
             mHandler.removeMessages(MSG_TOGGLE_CAMERA_FLASH);
@@ -828,6 +845,13 @@ public class CommandQueue extends IStatusBar.Stub {
                 case MSG_SHOW_PINNING_TOAST_ESCAPE:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).showPinningEscapeToast();
+                    }
+                    break;
+                case MSG_IN_DISPLAY_FINGERPRINT:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).handleInDisplayFingerprintView(
+                                (boolean)((SomeArgs)msg.obj).arg1,
+                                (boolean)((SomeArgs)msg.obj).arg2);
                     }
                     break;
                 case MSG_TOGGLE_CAMERA_FLASH:
